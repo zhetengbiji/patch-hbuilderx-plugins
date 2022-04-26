2e5c35faad5f45a2116e6830063dfbe3b2b43659
qiang <guoshengqiang@live.com>
2022-04-26 15:34:27 +0800

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# patch-hbuilderx-plugins
+
+## Init
+
+1. install deps
+
+```
+yarn init
+```
+
+2. create .env
+
+```
+HBUILDERX_PLUGINS_DIR="..."
+HBUILDERX_PLUGINS_PATHES_DIR="..."
+```
+
+## Making patches
+
+```
+yarn make --plugin %Plugin Name% %Package Names%
+```
+
+## Publish Patches
+
+> repo: https://github.com/zhetengbiji/hbuilderx-plugins-patches
+
+## Applying Patches
+
+* Developer
+
+```
+yarn patch %HBuilderX Path%
+```
+
+* User
+
+```
+npx patch-hbuilderx-plugins %HBuilderX Path%
+```
---
+++ scripts/make.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const dotenv = require('dotenv')
+dotenv.config()
+const minimist = require('minimist')
+const argv = minimist(process.argv.slice(2))
+const spawn = require('cross-spawn')
+
+const HBUILDERX_PLUGINS_DIR = process.env.HBUILDERX_PLUGINS_DIR
+const HBUILDERX_PLUGINS_PATHES_DIR = process.env.HBUILDERX_PLUGINS_PATHES_DIR
+const pluginName = argv.plugin
+const packageNames = argv._
+
+console.log(`pluginName: ${pluginName}`)
+console.log(`packageNames: ${packageNames}`)
+
+const cwd = path.join(HBUILDERX_PLUGINS_DIR, pluginName)
+const pathDir = path.relative(cwd, path.join(HBUILDERX_PLUGINS_PATHES_DIR, pluginName))
+
+const child = spawn('patch-package', packageNames.concat(['--patch-dir', pathDir]), {
+  stdio: 'inherit',
+  cwd
+})
+
+child.on('error', error => console.error(error))
+child.on('exit', () => console.log('Patch Done'))
